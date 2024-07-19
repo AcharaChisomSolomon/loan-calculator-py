0c00ffef4fc99cc23625f78c2b1033aefd1fdc6c
Achara_Chisom_Solomon <acharachisom861@gmail.com>
2024-07-19 09:53:06 +0100

    Task 2 done

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,7 +1,39 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
+import math
 
-# write your code here
+print('Enter the loan principal:')
+principal = int(input())
+
+print('''
+What do you want to calculate?
+type "m" - for number of monthly payments,
+type "p" - for the monthly payment:
+''')
+choice = input()
+
+if choice == 'm':
+    print('Enter the monthly payment:')
+    monthly_pay = int(input())
+
+    print()
+    time = math.ceil(principal / monthly_pay)
+    if time > 1:
+        print('It will take ' + str(time)
+              + ' months to repay the loan')
+    else:
+        print('It will take 1 month to repay the loan')
+
+elif choice == 'p':
+    print('Enter the number of months:')
+    months_num = int(input())
+    monthly_pay = principal / months_num
+    remainder = principal % months_num
+
+    print()
+    if remainder > 0:
+        monthly_pay = math.ceil(monthly_pay)
+        last_pay = principal - (months_num - 1) * monthly_pay
+        print('Your monthly payment = ', monthly_pay,
+              ' and the last payment = ', last_pay, '.',
+              sep='')
+    else:
+        print('Your monthly payment = ', int(monthly_pay))
